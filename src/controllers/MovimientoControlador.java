ae8864066509ae6a3998cddf364992a781b7402c
Ever-VC <ever.vasquezc27@gmail.com>
2024-10-21 15:09:49 -0600

    💥 NEW: Obtener movimientos de una cuenta especifica que esten dentro de un intervalo de tiempo especificado

--- src/controllers/MovimientoControlador.java
+++ src/controllers/MovimientoControlador.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.persistence.EntityManager;
+import models.Cuenta;
 import models.Empresa;
 import models.Movimiento;
 
@@ -39,9 +40,9 @@ public class MovimientoControlador {
         _entityManager.close();
     }
     
-    public List<Movimiento> GetPorFechaInicioYFin(Date fechaInicio, Date fechaFin, Empresa empresa) {
+    public List<Movimiento> GetMovimientosPorEmpresaYFechaInicioYFin(Date fechaInicio, Date fechaFin, Empresa empresa) {
         _entityManager = setEntityManager();
-        String jpql = "SELECT m FROM Movimiento m WHERE m.fecha > :fechaInicio AND m.fecha < :fechaFin AND m.idCuentaFk.idEmpresaFk = :empresa";
+        String jpql = "SELECT m FROM Movimiento m WHERE m.fecha >= :fechaInicio AND m.fecha <= :fechaFin AND m.idCuentaFk.idEmpresaFk = :empresa";
         
         List<Movimiento> movimientos = new ArrayList<>();
         try {
@@ -68,4 +69,32 @@ public class MovimientoControlador {
         return movimientos;
     }
     
+    public List<Movimiento> GetMovimientosPorCuentaYFechaInicioYFin(Date fechaInicio, Date fechaFin, Cuenta cuenta) {
+        _entityManager = setEntityManager();
+        String jpql = "SELECT m FROM Movimiento m WHERE m.fecha >= :fechaInicio AND m.fecha <= :fechaFin AND m.idCuentaFk = :cuenta";
+        
+        List<Movimiento> movimientos = new ArrayList<>();
+        try {
+            _entityManager.getTransaction().begin();
+
+            movimientos = _entityManager.createQuery(jpql, Movimiento.class)
+                        .setParameter("fechaInicio", fechaInicio)
+                        .setParameter("fechaFin", fechaFin)
+                        .setParameter("cuenta", cuenta)
+                        .getResultList();// Ejecuta la consulta y obtiene el resultado
+
+            _entityManager.getTransaction().commit();
+        } catch (Exception ex) {
+            if (_entityManager.getTransaction().isActive()) {
+                _entityManager.getTransaction().rollback();// Hace rollback en caso de error
+            }
+            ex.printStackTrace();// Imprime el error para depuración
+        } finally {
+            if (_entityManager != null) {
+                _entityManager.close();// Cierra el EntityManager
+            }
+        }
+
+        return movimientos;
+    }
 }
