52ea0b7a27da94d34f771716d0ba6a68731edc47
Ever-VC <ever.vasquezc27@gmail.com>
2024-11-02 10:14:26 -0600

    👷 CI:  Archivos de construcción

--- build/generated-sources/ap-source-output/models/DetalleAsiento_.java
+++ build/generated-sources/ap-source-output/models/DetalleAsiento_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 import models.Asiento;
 import models.Movimiento;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-10-22T08:04:05", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-11-02T10:09:49", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(DetalleAsiento.class)
 public class DetalleAsiento_ { 
 
--- build/generated-sources/ap-source-output/models/Empresa_.java
+++ build/generated-sources/ap-source-output/models/Empresa_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import models.Cuenta;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-10-22T08:04:05", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-11-02T10:09:49", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Empresa.class)
 public class Empresa_ { 
 
