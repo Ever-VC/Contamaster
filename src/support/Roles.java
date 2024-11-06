27d13fa5644674c60af1de18ff35daf819acef8d
Ever-VC <ever.vasquezc27@gmail.com>
2024-11-06 15:41:17 -0600

    💥 NEW: Definir acceso a partir de roles

---
+++ src/support/Roles.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package support;
+
+/**
+ *
+ * @author ever_vc
+ */
+public class Roles {
+    
+    public Roles() {}
+    
+    public final String ADMINISTRADOR = "Administrador";
+    public final String CONTADOR = "Contador";
+    public final String AUXILIAR = "Auxiliar";
+    public final String GERENTE = "Gerente";
+}
